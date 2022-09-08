3d614580be468814c165f5d64441d7869e80ddf7
r <s>
2022-09-08 16:07:21 +0530

    Simple Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  count = 1;
+  num1 = 0;
+  num2 = 0;
+  sum = 0
+  addition = () => {
+    this.sum = this.num1 + this.num2;
+  }
+  subtraction = () => {
+    this.sum = this.num1 - this.num2;
+  }
+  division = () => {
+    this.sum = this.num1 / this.num2;
+  }
+  multiplication = () => {
+    this.sum = this.num1 * this.num2;
+  }
+  power = () => {
+    this.sum = Math.pow(this.num1, this.num2);
+  }
+
+
+
 }
+
